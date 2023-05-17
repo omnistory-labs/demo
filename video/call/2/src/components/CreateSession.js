f2c0f1ba0e7cd95ddd6e220c54c3ea0dc66c55a1
luna <luna@omnistory.net>
2023-05-17 18:39:59 +0900

    Modify: React Video Call Demo

--- video/call/2/src/components/CreateSession.js
+++ video/call/2/src/components/CreateSession.js
@@ -12,46 +12,32 @@ import { CreateSessionBtn, DisabledBtn } from "../style/DemoBtn";
 export default function CreateSession({
   handleCreateSession,
   handleChange,
-  isValid,
+  regiNum,
 }) {
   return (
     <StyledContents>
       <form className="call_form">
-        <h3>Video Call</h3>
-        <div className="dot_wrap">
-          <span className="dot_active"> </span>{" "}
-          <span className="hyphen"> </span>
-          <span className="dot"> </span> <span className="hyphen"> </span>
-          <span className="dot"> </span> <span className="hyphen"> </span>
-          <span className="dot"> </span>
-        </div>
-        <label htmlFor="createSession">번호 등록</label>
-        <input
-          type="text"
-          name="createSession"
-          placeholder="번호를 등록해 주세요."
-          onChange={handleChange}
-        />
-        <p>
-          {" "}
-          <span>* </span>특수문자를 제외한 숫자, 문자, 이메일 주소 등을
-          입력하세요.
-        </p>
-        <div className="camera_icon">
-          <video />
-          <img
-            src="https://user-images.githubusercontent.com/120351058/217710092-49584d9b-0798-427f-bc5c-6e0ce1285625.png"
-            alt="camera"
+        <h2>Video Call</h2>
+        <div className="createSession_wrap">
+          <label htmlFor="createSession">{/* Step 1. 내 번호 등록 */}</label>
+          <input
+            type="text"
+            name="createSession"
+            placeholder="Please register the number."
+            onChange={handleChange}
           />
+          <p>
+            <span>* </span>특수문자를 제외한 숫자, 문자, 이메일 주소 등을
+            입력하세요.
+          </p>
+          <div className="create_button">
+            <CreateSessionBtn
+              text="번호등록"
+              regiNum={regiNum}
+              handleCreateSession={handleCreateSession}
+            />
+          </div>
         </div>
-        {!isValid ? (
-          <DisabledBtn text="번호등록" disabled />
-        ) : (
-          <CreateSessionBtn
-            text="번호등록"
-            handleCreateSession={handleCreateSession}
-          />
-        )}
       </form>
     </StyledContents>
   );
@@ -59,111 +45,30 @@ export default function CreateSession({
 const StyledContents = styled.div`
   width: 100%;
   .call_form {
-    width: 800px;
-    height: 800px;
     margin: 0 auto;
-    padding: 40px;
-    border-radius: 15px;
     background-color: ${palette.white};
-    box-shadow: ${palette.shadow};
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 2px 0px;
     position: relative;
     overflow: hidden;
-    h3 {
+    h2 {
       text-align: center;
+      padding-top: 50px;
+      padding-bottom: 30px;
       margin-bottom: 20px;
       font-size: 1.5rem;
-      font-weight: ${fontWeight.medium};
+      border-bottom: 1px solid ${palette.gray.middle};
     }
-    .dot_wrap {
-      margin-bottom: 40px;
-      ${defaultFlexCenter}
-      .dot {
-        display: inline-block;
-        width: 10px;
-        height: 10px;
-        margin: 0;
-        border-radius: 100px;
-        background-color: ${palette.gray.bright};
-      }
-      .dot_active {
-        display: inline-block;
-        width: 10px;
-        height: 10px;
-        margin: 0;
-        border-radius: 100px;
-        background-color: ${palette.main.default};
-      }
-      .hyphen {
-        display: inline-block;
-        width: 20px;
-        height: 2px;
-        margin: 0;
-        background-color: ${palette.gray.bright};
-      }
-      .hyphen_active {
-        display: inline-block;
-        width: 20px;
-        height: 2px;
-        margin: 0;
-        background-color: ${palette.main.default};
-      }
-    }
-    label,
-    h4 {
-      display: inline-block;
-      width: 100%;
-      margin: 0;
-      margin-bottom: 20px;
-      text-align: start;
-      font-weight: ${fontWeight.regular};
-      font-size: ${fontSize.regular};
-    }
-    input {
-      ${defaultInput}
-      width: 100%;
-      padding-left: 10px;
-      border: 0;
-      background-color: ${palette.gray.boxColor};
-      ::placeholder {
-        font-size: ${fontSize.micro};
-      }
-    }
-    p {
-      padding-left: 14px;
-      position: relative;
-      color: ${palette.text.default};
-      text-align: start;
-      span {
-        position: absolute;
-        top: 0;
-        left: 0;
-        font-size: ${fontSize.medium};
-        color: ${palette.main.default};
-      }
-    }
-    .camera_icon {
-      position: relative;
-      video {
+    .createSession_wrap {
+      width: 400px;
+      margin: 0 auto;
+      input {
+        ${defaultInput}
         width: 100%;
-        background-color: #d9d9d9;
-      }
-      img {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-      }
-    }
-  }
-
-  @media screen and (max-width: 980px) and (min-width: 230px) {
-    width: 100%;
-    .call_form {
-      width: 100%;
-      padding: 30px;
-      label,
-      h4 {
-        font-size: 1.1rem;
+        padding-left: 10px;
+        margin-top: 30px;
+        ::placeholder {
+          font-size: 16px;
+        }
       }
       p {
         padding-left: 14px;
@@ -178,19 +83,27 @@ const StyledContents = styled.div`
           color: ${palette.main.default};
         }
       }
-      .camera_icon {
-        position: relative;
-        video {
-          width: 100%;
-          height: 400px;
-          background-color: #d9d9d9;
-        }
-        img {
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-        }
+    }
+    .create_button {
+      width: 400px;
+    }
+  }
+
+  @media screen and (max-width: 580px) and (min-width: 230px) {
+    width: 100%;
+    .call_form {
+      h2 {
+        padding-top: 30px;
+      }
+      .createSession_wrap {
+        width: 100%;
+        padding: 0 5%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+      .create_button {
+        width: 100%;
       }
     }
   }
